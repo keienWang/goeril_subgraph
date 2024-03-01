b6b306456298170274d2c983c9e3b2be10233e9f
keienWang <42377006+keienWang@users.noreply.github.com>
2024-03-01 16:21:09 +0800

    1

--- src/eth-tornado.ts
+++ src/eth-tornado.ts
@@ -5,32 +5,36 @@ import {
 import { Deposit, Withdrawal } from "../generated/schema"
 
 export function handleDeposit(event: DepositEvent): void {
-  let entity = new Deposit(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.commitment = event.params.commitment
-  entity.leafIndex = event.params.leafIndex
-  entity.timestamp = event.params.timestamp
+  let entity = new Deposit(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
+  entity.amount ="0.1";
+  entity.currency = "eth";
+
+  entity.from = event.transaction.from;
+  entity.index = event.params.leafIndex;
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.commitment = event.params.commitment;
+  entity.transactionHash = event.transaction.hash;
+
+  
   entity.save()
 }
 
 export function handleWithdrawal(event: WithdrawalEvent): void {
-  let entity = new Withdrawal(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.to = event.params.to
-  entity.nullifierHash = event.params.nullifierHash
-  entity.relayer = event.params.relayer
-  entity.fee = event.params.fee
+  let entity = new Withdrawal(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.amount = "0.1";
+  entity.currency = "eth";
+
+  entity.to = event.params.to;
+  entity.fee = event.params.fee;
+  entity.index = event.logIndex;
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.nullifier = event.params.nullifierHash;
+  entity.transactionHash = event.transaction.hash;
 
   entity.save()
 }
